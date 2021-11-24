66314ee072d0fc79d33fece4f4d4554a3c2e6b50
abhilash0717 <40905548+abhilash0717@users.noreply.github.com>
2021-11-24 20:52:45 +0530

    restoring front end files

---
+++ api-demo/src/App.js
@@ -0,0 +1,13 @@
+import ApiData from "./ApiData";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <ApiData />
+    </div>
+  );
+}
+
+export default App;
